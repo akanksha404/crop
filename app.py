5137171fae6d02511c96fd4a8882067817e92df9
Akanksha 404 <goduguakanksha9@gmail.com>
2024-01-10 17:34:55 +0530

    finish

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 #backend of the app
 from flask import Flask,render_template,jsonify,request
 import pickle
+import sqlite3    #to connect the table and queries
 app = Flask(__name__)
 
 @app.route('/') #for home url empty /
@@ -21,9 +22,34 @@ def prediction():
             mlmodel=pickle.load(model_file)
         res = mlmodel.predict([[float(nitro),float(phos),float(pott),float(temp),float(hum),float(ph),float(rain)]])
         #jsonify will return to frontend file
+        print(res)
+        conn = sqlite3.connect('cropdatabase.db')
+        cur = conn.cursor()
+        cur.execute(f'''INSERT INTO CROPS VALUES({nitro},{phos},{pott},{temp},{hum},{ph},{rain},'{res[0]}')''')
+        conn.commit()
         return render_template("result.html",res=res[0])
     else:
         return render_template('prediction.html')
+@app.route('/showdata.html',methods =['GET','POST'])
+def showdata():
+    conn = sqlite3.connect('cropdatabase.db')
+    cur = conn.cursor()
+    cur.execute("SELECT * FROM CROPS")
+    x=cur.fetchall()
+    li=[]
+    for i in x:
+        p={}
+        p['Nitrogen']=i[0]
+        p['Phosphorous']=i[1]
+        p['Potassium']=i[2]
+        p['Temperature']=i[3]
+        p['Humidity']=i[4]
+        p['Ph']=i[5]
+        p['Rainfall']=i[6]
+        p['Result']=i[7]
+        li.append(p)
+
+    return render_template('showdata.html',data = li)
 
 
 
